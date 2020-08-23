acc7512e66807de3d056860d8a82bb15adb81bc4
anathayna <anathaynafranca@gmail.com>
2020-08-22 22:35:32 -0300

    now it works

--- 3-elm/1-basics/src/main.elm
+++ 3-elm/1-basics/src/main.elm
@@ -1,3 +1,22 @@
 module Main exposing (..)
+import Browser
+import Html exposing(div, text)
 
-add a b = a + b
+init =
+    { value = 0 }
+
+view model =
+    div [] [text "hello!"]
+
+update model =
+    model
+
+main =
+    Browser.sandbox
+        {
+            init = init
+            , view = view
+            , update = update
+        }
+
+-- elm reactor to build and run (control + c to exit)
