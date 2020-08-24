6de09bb63fe021f534ab5eff262cebe486c3673b
anathayna <anathaynafranca@gmail.com>
2020-08-24 19:15:59 -0300

    adding more basics examples

--- 3-lua/1-basics/main.lua
+++ 3-lua/1-basics/main.lua
@@ -10,3 +10,17 @@ print(false or 13)
 print(10 > 93)
 
 -- lua main.lua build & run
+
+var1 = "mundo da lua!"
+var2 = 20
+
+print(type(var1))
+print(type(var2))
+
+local multistring = [[
+	Hello. This is a very long string 
+	that is declared using double square 
+	brackets during the assignment.
+]]
+
+print(multistring)
