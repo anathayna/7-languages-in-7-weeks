73612b4ab84db45f0e5d146a3000ecd3d546e4ea
anathayna <anathaynafranca@gmail.com>
2020-08-30 22:14:21 -0300

    finish finish lua type exercice

--- 3-lua/3-types/main.lua
+++ 3-lua/3-types/main.lua
@@ -3,8 +3,8 @@
 function dna(cadeia, tabela)
   for i=1 , #cadeia do
     -- local c = cadeia:sub(i,i)
-    local x = string.sub(cadeia,i,i)
-    tabela[x] = tabela[x] + 1
+    local c = string.sub(cadeia,i,i)
+    tabela[c] = tabela[c] + 1
   end
 end
 
