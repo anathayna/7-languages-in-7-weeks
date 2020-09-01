246d73be4755ef34017e3cf90034b252f0798c20
anathayna <anathaynafranca@gmail.com>
2020-08-31 21:27:17 -0300

    adding rust function example

---
+++ 4-rust/e-function/src/main.rs
@@ -0,0 +1,15 @@
+fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+    // corner case, early return
+    if rhs == 0 {
+        return false;
+    }
+
+    // this is an expression, the `return` keyword is not necessary here
+    lhs % rhs == 0
+}
+
+fn main() {
+    if is_divisible_by(100,10) {
+        println!("é divisível");
+    }
+}
