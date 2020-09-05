375e0fd11ad20fd22fc4dd5969de542798fe2aeb
anathayna <anathaynafranca@gmail.com>
2020-09-05 20:41:41 -0300

    rename project

--- 4-rust/f-command-line/count/src/main.rs
+++ 4-rust/f-command-line/count/src/main.rs
@@ -2,7 +2,7 @@
 //https://docs.rs/structopt/0.3.17/structopt/
 //https://doc.rust-lang.org/std/string/struct.String.html
 
-// counts são int
+// conts são int
 // qual o melhor escolher? i8, i16, i32... justifique c/ comentário
 
 use structopt::StructOpt;
@@ -33,5 +33,5 @@ fn main() {
     // Ok(())
 }
 
-// count -l ~/count.txt exibe a linha -> wc -l count.txt
-// count -w ~/count.txt exibe a qnt de palavras -> wc -w count.txt 
+// cont -l ~/cont.txt exibe a linha -> wc -l cont.txt
+// cont -w ~/cont.txt exibe a qnt de palavras -> wc -w cont.txt 
