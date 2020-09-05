8745edab66f3b30c4105256dc171a88c8771b7e0
anathayna <anathaynafranca@gmail.com>
2020-09-05 18:34:39 -0300

    install dependencies

--- 4-rust/f-command-line/cont/src/main.rs
+++ 4-rust/f-command-line/cont/src/main.rs
@@ -1,3 +1,25 @@
+//https://rust-cli.github.io/book/index.html
+//https://docs.rs/structopt/0.3.17/structopt/
+//https://doc.rust-lang.org/std/string/struct.String.html
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli{
+    padrao: String,
+    #[structopt(parse(from_os_str))]
+    arquivo: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    let content = std::fs::read_to_string(&args.arquivo)
+    .expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.padrao) {
+            println!("{}", line);
+        }
+    }
 }
