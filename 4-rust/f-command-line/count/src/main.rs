74c8d38fa67a070ec695f823b3a4d06387d62478
anathayna <anathaynafranca@gmail.com>
2020-09-12 18:11:05 -0300

    finish count exercice

--- 4-rust/f-command-line/count/src/main.rs
+++ 4-rust/f-command-line/count/src/main.rs
@@ -1,77 +1,36 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::path::Path;
-use std::env;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli{
+    padrao: String,
+    #[structopt(parse(from_os_str))]
+    arquivo: std::path::PathBuf,
+}
 
 // i32: p/ melhor utilização de cache nesse exemplo, ocupando só 8K de dados
-fn count_words(s: &String) -> i32 {
-    let mut words: i32 = 0;
-
-    for c in s.chars() {
-        if c.is_whitespace() {
-            words += 1;    
-        }   
-    }
-    words + 1
-}
-
-fn counter<R: BufRead> (reader: &mut R) -> Result<(i32, i32), String> {
-    let mut total_lines: i32 = 0;
-    let mut total_words: i32 = 0;
-    let mut line = String::from("");
-
-    loop{ 
-        match reader.read_line(&mut line) {
-            Ok(_) => {
-                if line.len() == 0 {
-                    break;
-                }
-                
-                line = line.trim().to_string();
-                total_lines += 1;
-                total_words += count_words(&line);
-                line.clear(); 
-            },
-            Err(why) => return Err(why.to_string())
-        };
-    }
-    Ok((total_lines, total_words))
-}
-
-fn count_file(file_path: &Path) -> Result< (i32,i32), String> {
-    let file_handle = match File::open(&file_path) {
-        Err(why) => return Err(why.to_string()),
-        Ok(file_handle) => file_handle
-    };
-
-    let mut reader = BufReader::new(file_handle);
-    let (lines, words) = counter(&mut reader)?;
-    Ok((lines, words))
-}
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let files: Vec<String> = Vec::from(&args[1..]);
+    let mut words: i32 = 0 ;
+    let mut lines: i32 = 0;
+    let args = Cli::from_args();
 
-    if args.len() < 2 {
-        panic!("could not read file");            
-    } 
+    let content = std::fs::read_to_string(&args.arquivo)
+    .expect("could not read file");
 
-    for file_name in files.iter() {
-        let path = Path::new(&file_name); 
+    for line in content.lines() {
+        if line.contains(&args.padrao) {
+            println!("{}", line);
+        }
 
-        match count_file(path) {
-            Ok((lines, words)) => {
-                println!("{}\t{} lines\t{} words.", path.display(), lines, words);
-            },
-            Err(err) => {
-                panic!("error - {}", err);
-            }
-        };
+        let x = line.split(" ");
+
+        lines += 1;
+        words += x.count() as i32;
     }
+
+    println!("{}\t lines", lines);
+    println!("{}\t words", words);
 }
 
 // wc -l count.txt		
 // wc -w count.txt
-// cargo run ./count.txt 
+// cargo run count count.txt
---
+++ 4-rust/f-command-line/hard/src/main.rs
@@ -0,0 +1,76 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
+use std::env;
+
+fn count_words(s: &String) -> i32 {
+    let mut words: i32 = 0;
+
+    for c in s.chars() {
+        if c.is_whitespace() {
+            words += 1;    
+        }   
+    }
+    words + 1
+}
+
+fn counter<R: BufRead> (reader: &mut R) -> Result<(i32, i32), String> {
+    let mut total_lines: i32 = 0;
+    let mut total_words: i32 = 0;
+    let mut line = String::from("");
+
+    loop{ 
+        match reader.read_line(&mut line) {
+            Ok(_) => {
+                if line.len() == 0 {
+                    break;
+                }
+                
+                line = line.trim().to_string();
+                total_lines += 1;
+                total_words += count_words(&line);
+                line.clear(); 
+            },
+            Err(why) => return Err(why.to_string())
+        };
+    }
+    Ok((total_lines, total_words))
+}
+
+fn count_file(file_path: &Path) -> Result< (i32,i32), String> {
+    let file_handle = match File::open(&file_path) {
+        Err(why) => return Err(why.to_string()),
+        Ok(file_handle) => file_handle
+    };
+
+    let mut reader = BufReader::new(file_handle);
+    let (lines, words) = counter(&mut reader)?;
+    Ok((lines, words))
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let files: Vec<String> = Vec::from(&args[1..]);
+
+    if args.len() < 2 {
+        panic!("could not read file");            
+    } 
+
+    for file_name in files.iter() {
+        let path = Path::new(&file_name); 
+
+        match count_file(path) {
+            Ok((lines, words)) => {
+                println!("{}\t{} lines\t{} words.", path.display(), lines, words);
+            },
+            Err(err) => {
+                panic!("error - {}", err);
+            }
+        };
+    }
+}
+
+// wc -l hard.txt		
+// wc -w hard.txt
+// cargo run ./hard.txt 
