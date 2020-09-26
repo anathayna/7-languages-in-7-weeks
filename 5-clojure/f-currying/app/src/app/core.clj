02d9453477cbc2fd7dd2249a65dbe9f34423e279
anathayna <anathaynafranca@gmail.com>
2020-09-26 15:02:58 -0300

    adding currying

---
+++ 5-clojure/f-currying/app/src/app/core.clj
@@ -0,0 +1,16 @@
+(ns f-currying.core
+  (:gen-class))
+
+(defn -main
+  "i don't do a whole lot ... yet."
+  [& args]
+
+  (println "------ função parcial ----")
+  (defn soma [a b] (+ a b))
+  (defn soma2 [b] (partial (soma 2)))
+
+  (println ((soma2 10))
+  (println ((soma2 50))
+
+  (println (map (partial reduce +) [[1 2 3 4] [5 6 7 8]]))))
+)
