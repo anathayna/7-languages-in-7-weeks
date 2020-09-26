5cd795f6bc5dd1c9837e35a89173e13604773df3
anathayna <anathaynafranca@gmail.com>
2020-09-26 14:57:01 -0300

    adding pattern matching examples

---
+++ 5-clojure/e-pattern-matching/app/src/app/core.clj
@@ -0,0 +1,24 @@
+(ns e-pattern-matching.core
+  (:gen-class)
+  (:require [clojure.core.match :refer [match]])
+  )
+
+(defn -main
+  [& args]
+
+  ; pattern matching
+  (println "------- pattern matching -------")
+  (doseq [n (range 1 10)]
+    (println
+      (match [(mod n 3) (mod n 5)]
+        [0 0] "FizzBuzz"
+        [0 _] "Fizz"
+        [_ 0] "Buzz"
+        :else n)))
+  
+  ; binding
+  (println "------- binding -------")
+  (def tabuleiro [[:x :o :x] [:x :o :x] [:x :o :x]])
+  (defn central [ [_ [_ c _]_]  ] c)
+  (println (central tabuleiro))
+)
