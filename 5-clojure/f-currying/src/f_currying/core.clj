bed5dbe661305b2ff12dd508c13980efa8faf988
anathayna <anathaynafranca@gmail.com>
2020-09-26 19:25:28 -0300

    fixing currying example

--- 5-clojure/f-currying/src/f_currying/core.clj
+++ 5-clojure/f-currying/src/f_currying/core.clj
@@ -2,15 +2,15 @@
   (:gen-class))
 
 (defn -main
-  "i don't do a whole lot ... yet."
   [& args]
+  "i don't do a whole lot ... yet."
 
-  (println "------ função parcial ----")
-  (defn soma [a b] (+ a b))
-  (defn soma2 [b] (partial (soma 2)))
+  (println "------ função parcial ------")
+  (defn somadora [a b] (+ a b))
+  (def soma (partial somadora 10))
 
-  (println ((soma2 10))
-  (println ((soma2 50))
+  (println (soma 5))
+  (println (soma 10))
 
-  (println (map (partial reduce +) [[1 2 3 4] [5 6 7 8]]))))
+  (println (map (partial reduce +) [[1 3 4] [3 5 6]]))
 )
