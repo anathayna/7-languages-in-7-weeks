354c486ce4a220c573b680b348a45919a398a942
anathayna <anathaynafranca@gmail.com>
2020-09-28 21:28:04 -0300

    adding contains example

--- 5-clojure/f-currying/src/f_currying/core.clj
+++ 5-clojure/f-currying/src/f_currying/core.clj
@@ -13,4 +13,19 @@
   (println (soma 10))
 
   (println (map (partial reduce +) [[1 3 4] [3 5 6]]))
+
+  (println "------ contains ------")
+  (def lista-ids [1 2 3 5 6 7 8 9 10])
+  (def usaram-recurso-ids #{3 4 5 9}) ; #{} => Conjunto
+
+  ; dica: fazer uma função que checa se um item da lista está no conjunto usando contains?
+  ; https://clojuredocs.org/clojure.core/contains_q
+
+
+  (defn quem-usou-recurso 
+    [lista-todos lista-usaram]
+    (map (partial contains? lista-usaram) lista-todos)
+  )
+
+  (println (quem-usou-recurso lista-ids usaram-recurso-ids))
 )
