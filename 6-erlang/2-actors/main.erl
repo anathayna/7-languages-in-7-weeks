7a4b44663ed00aa50b7cc67f3691d191a3151520
ana thayna <anathaynafranca@gmail.com>
2020-10-26 20:41:44 -0300

    add erlang actors

---
+++ 6-erlang/2-actors/main.erl
@@ -0,0 +1,37 @@
+-module(main).
+-export([start/0]).
+-import(timer,[sleep/1]). % importação do timer
+
+calculadora() ->
+  receive
+    {soma, M, A, B} -> 
+      io:format("~p~n", [A + B]),
+      M ! {salvar, A + B},
+      calculadora();
+    {subtracao, M, A, B} -> 
+      io:format("~p~n", [A - B]),
+      M ! {salvar, A - B},
+      calculadora();
+    _ -> 
+      io:format("Nao entendi~n"),
+      calculadora()
+  end.
+
+% closure
+memoria(Z) ->
+  receive
+    {salvar, V} -> memoria(V);
+    imprimir -> 
+      io:format("Valor: ~p~n", [Z]),
+      memoria(Z);
+    _ -> 
+      io:format("Nao entendi~n"),
+      memoria(Z)
+  end.
+
+start() -> 
+  C1 = spawn(fun() -> calculadora() end), % pid
+  M = spawn(fun() -> memoria(0) end), % pid
+  C1 ! {soma, M, 7, 5}, %async
+  sleep(100), 
+  M ! imprimir.
