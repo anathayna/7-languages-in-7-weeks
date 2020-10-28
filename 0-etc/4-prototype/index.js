b78f8ff6389dfbd99a1b1064ade7f2abff5067eb
ana thayna <anathaynafranca@gmail.com>
2020-10-27 22:40:34 -0300

    add constructor

--- 0-etc/4-prototype/index.js
+++ 0-etc/4-prototype/index.js
@@ -1,8 +1,21 @@
-let usuario  = {
-    nome: "bob",
+let user  = {
+    name: "bob",
     email: "bob@obob.com",
-    imprime: function() { return `${this.nome} - ${this.email}`; }
+    print: function() { return `${this.name} - ${this.email}`; }
   };
   
-  console.log(usuario.nome);
-  console.log(usuario.imprime());
+  console.log(user.name);
+  console.log(user.print());
+  user.idade = 13;
+  user.printage = function() { return `${this.idade} anos` }
+  console.log(user.printage());
+  
+  //construtor
+  function User(name, email) {
+    this.name = name;
+    this.email = email;
+    this.print = function() { return `${this.name} - ${this.email}`; }
+  }
+  
+  let user2 = new User("amy", "amy@yma.com");
+  console.log(user2.print());
