62d4680f4ec3d4e089e2ceec5ae61a14d2904756
ana thayna <anathaynafranca@gmail.com>
2020-11-09 19:55:20 -0300

    add prolog examples

---
+++ 7-prolog/a-basics/main.pl
@@ -0,0 +1,12 @@
+
+gosta_de(jose, maria).
+gosta_de(jose, vanessa).
+gosta_de(maria, jose).
+gosta_de(maria, vinicius).
+gosta_de(vanessa, maria).
+gosta_de(vanessa, vinicus).
+gosta_de(vinicius, vanessa).
+
+% consulta
+% variáveis são átomos em maiúsculos
+% gosta_de(maria, X).
---
+++ 7-prolog/b-types/main.pl
@@ -0,0 +1,17 @@
+% base de conhecimento
+gosta_de(jose, maria).
+gosta_de(jose, vanessa).
+gosta_de(maria, jose).
+gosta_de(maria, vinicius).
+gosta_de(vanessa, maria).
+gosta_de(vanessa, vinicus).
+gosta_de(vinicius, vanessa).
+
+% regras
+amizade(X,Y) :- gosta_de(X, Y), gosta_de(Y, X).
+
+% consultar
+% amizade(maria,jose).
+% amizade(maria,vinicius).
+% amizade(vanessa,Y).
+% amizade(X,Y).
