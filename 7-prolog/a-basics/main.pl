4d8cd2ddf355653f01b7e38c9a2fb2b366f05979
ana thayna <anathaynafranca@gmail.com>
2020-11-09 20:34:28 -0300

    edit prolog basics

--- 7-prolog/a-basics/main.pl
+++ 7-prolog/a-basics/main.pl
@@ -1,12 +1,22 @@
-
+% base de conhecimento
 gosta_de(jose, maria).
 gosta_de(jose, vanessa).
 gosta_de(maria, jose).
 gosta_de(maria, vinicius).
 gosta_de(vanessa, maria).
-gosta_de(vanessa, vinicus).
+gosta_de(vanessa, vinicius).
 gosta_de(vinicius, vanessa).
+gosta_de(vinicius, maria).
 
-% consulta
+% consulta gprolog ['main.pl'].
 % variáveis são átomos em maiúsculos
 % gosta_de(maria, X).
+
+% regras
+amizade(X,Y) :- gosta_de(X, Y), gosta_de(Y, X).
+
+% consultar
+% amizade(maria,jose).
+% amizade(maria,vinicius).
+% amizade(vanessa,Y).
+% amizade(X,Y).
