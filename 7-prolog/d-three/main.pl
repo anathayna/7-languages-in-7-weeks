aedbec785cc11d26e1fdc00dc74b9bf547d63574
ana thayna <anathaynafranca@gmail.com>
2020-11-25 00:10:32 -0300

    finish finish

--- 7-prolog/d-three/main.pl
+++ 7-prolog/d-three/main.pl
@@ -1,19 +1,23 @@
 mae(maria, joana).
 mae(joana, luis).
 mae(cleia, otavio).
+mae(ana, pedro).
 pai(orozimbo, otavio).
 pai(otavio, luis).
+pai(orozimbo, antonio).
+pai(joao, joana).
 irmao(orozimbo, manuel).
 irmao(orozimbo, adelia).
+irmao(joana, ana).
  
 ancestral(X, Y) :- mae(X, Y); pai(X, Y).  
-vovos(X, Y) :- mae(X, A); pai(X, A), ancestral(A, Y). 
-sogrx(X, Y) :- pai(X, A) , mae(B, A) , mae(Y, B) ; pai(Y, B) ; pai(B, A), mae(X, A), mae(Y, B) ; pai(Y, B).
+vovos(X, Y) :- (mae(X, A); pai(X, A)), ancestral(A, Y). 
+sogrx(X, Y) :- pai(X, A) , mae(B, A), (mae(Y, B) ; pai(Y, B)) ; pai(B, A), mae(X, A), (mae(Y, B) ; pai(Y, B)).
 irmaos(X, Y) :- irmao(Y, X) ; irmao(X, Y), \+ (Y = X).
-cunhadx(X, Y) :- pai(X, A), mae(B, A), irmaos(B, Y) ; pai(B, A), mae(X, A), irmaos(B, Y).
+cunhadx(X, Y) :- pai(X, A), mae(B, A), irmaos(B, Y) ; (pai(B, A), mae(X, A), irmaos(B, Y)).
 tio(X, Y) :- pai(A, X), irmaos(A, Y).
-primos(X, Y) :- tios(X, A), pai(A, Y) ; mae(A, Y).
-tioavo(X, Y) :- avoa(A, Y), irmaos(A, X).
+primos(X, Y) :- tio(X, A), (pai(A, Y) ; mae(A, Y)).
+tioavo(X, Y) :- vovos(A, Y), irmaos(A, X).
 
 % consulta gprolog ['main'].
 % ancestral(maria, luis). true.
