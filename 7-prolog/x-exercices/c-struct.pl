9c9092914405eb4365e091c46955f1be0c484f9e
ana thayna <anathaynafranca@gmail.com>
2020-12-06 14:04:08 -0300

    finish struct exercice

--- 7-prolog/x-exercices/c-struct.pl
+++ 7-prolog/x-exercices/c-struct.pl
@@ -1,8 +1,9 @@
 % ultimo(L,X). [1, 2, 4, 5] => X = 5
 
-ultimo([X|[]],X).
-lista is [1,2,3,45].
+ultimo_elemento([X | []], X).
 
-ultimo_elemento(L, X) :- ultimo(L, [X | []], X).
-% cada item da lista
-% descer com o tail
+% ultimo_elemento(elemento, lista)
+ultimo_elemento(E, [_|X]) :- ultimo_elemento(E, X).
+
+% consulta gprolog ['c-struct'].
+% ultimo_elemento(E, [1,2,4,5])
--- 7-prolog/x-exercices/d-arith.pl
+++ 7-prolog/x-exercices/d-arith.pl
@@ -1,3 +1,6 @@
 % dada uma lista L de números, como calculo o maximo N?
 % L = [0, 10, 3, -2], N = 10
 % max(N,L). 
+
+max([X,Y|T],M) :- X >= Y, !, max([X|T], M).
+max([X,Y|T],M) :- max([Y|T], M).
